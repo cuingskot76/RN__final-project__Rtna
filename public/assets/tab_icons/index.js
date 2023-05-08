aeed265d685e04f1e88fe1477716dd1803252301
cuingskot76 <afrizapamujo@gmail.com>
2023-05-08 22:44:15 +0700

    feat: add navigation

---
+++ public/assets/tab_icons/index.js
@@ -0,0 +1,17 @@
+import HomeIcon from './home.svg';
+import HomeIconActive from './homeActive.svg';
+import SearchIcon from './search.svg';
+import NotificationIcon from './notification.svg';
+import NotificationIconActive from './notificationActive.svg';
+import ProfileIcon from './profile.svg';
+import ProfileIconActive from './profileActive.svg';
+
+export {
+  HomeIcon,
+  HomeIconActive,
+  SearchIcon,
+  NotificationIcon,
+  NotificationIconActive,
+  ProfileIcon,
+  ProfileIconActive,
+};
