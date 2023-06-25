a95a6a84015c0523b43cb592f034648ab144a5b9
cuingskot76 <afrizapamujo@gmail.com>
2023-06-25 17:26:13 +0700

    feat: add Keep items

--- src/screens/keep/Keep.jsx
+++ src/screens/keep/Keep.jsx
@@ -1,6 +1,9 @@
-import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import {View, Text, TextInput, TouchableOpacity, Image} from 'react-native';
 import React, {useState} from 'react';
 import {SearchIcon} from '../../../public/assets/icons';
+
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Keep = () => {
   // state untuk inputan pada text input
@@ -12,9 +15,43 @@ const Keep = () => {
     setInput(e);
   };
 
+  const datas = [
+    {
+      id: 1,
+      title: 'Perjanjian dengan setan',
+      time: '20:00 - 21:00',
+      img: 'https://i.scdn.co/image/a63dca57c3fb796f6ae05ee9695a7fbc6b5e595c',
+    },
+    {
+      id: 2,
+      title: 'Genderuwo dikatain',
+      time: '22:00',
+      img: 'https://i.scdn.co/image/ab6765630000ba8a0b9f43cdbe95f450389cb32f',
+    },
+    {
+      id: 3,
+      title: 'Kuntilanak merah jambu',
+      time: '23:00',
+      img: 'https://i.scdn.co/image/ab6765630000ba8a95665b58c922ad046ac2e7c9',
+    },
+    {
+      id: 4,
+      title: 'Tumbal perempuan',
+      time: '21:00 - 21:30',
+      img: 'https://i.scdn.co/image/ab6765630000ba8a84a1ccecb0e48f742533d5e0',
+    },
+    {
+      id: 5,
+      title: 'Cerita tengah malam',
+      time: '24:00',
+      img: 'https://i.scdn.co/image/ab6765630000ba8a99fb0d9d10ecab650ed5d3ea',
+    },
+  ];
+
   return (
     <View
       style={{width: '100%', flex: 1, padding: 20, backgroundColor: '#fff'}}>
+      {/* input */}
       <View
         style={{
           display: 'flex',
@@ -49,16 +86,62 @@ const Keep = () => {
         </TouchableOpacity>
       </View>
 
-      <Text
-        style={{
-          alignContent: 'center',
-          color: 'salmon',
-          alignItems: 'center',
-          fontWeight: 'bold',
-          fontSize: 20,
-        }}>
-        Maaf, Belum ada Playlist ditambahkan!
-      </Text>
+      <View>
+        {
+          // menampilkan data dari state datas
+          datas.map(data => (
+            <TouchableOpacity
+              key={data.id}
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                marginTop: 20,
+                gap: 20,
+              }}>
+              <View>
+                <Image
+                  source={{uri: data.img}}
+                  style={{
+                    width: 60,
+                    height: 60,
+                    borderRadius: 10,
+                    resizeMode: 'cover',
+                  }}
+                  alt={`${data.title} image`}
+                />
+              </View>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  // backgroundColor: 'lightblue',
+                  width: '75%',
+                  justifyContent: 'space-between',
+                }}>
+                <View>
+                  <Text style={{fontSize: 16, fontWeight: 'bold'}}>
+                    {data.title}
+                  </Text>
+                  <Text style={{fontSize: 14, color: '#9FA5C0'}}>
+                    {data.time}
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    gap: 10,
+                  }}>
+                  <TouchableOpacity>
+                    <AntDesign name="play" size={25} color="black" />
+                  </TouchableOpacity>
+                  <TouchableOpacity>
+                    <Ionicons name="trash-outline" size={25} color="black" />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </TouchableOpacity>
+          ))
+        }
+      </View>
     </View>
   );
 };
